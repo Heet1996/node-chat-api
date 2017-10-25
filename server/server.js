43bae913e568d680b8f588476995d54714515148
Heet1996 <heet1@live.com>
2017-10-25 16:09:30 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+var app = express();
+const publicPath=path.join(__dirname,'../public');
+app.use(express.static(publicPath));
+app.listen(3000,()=>{
+  console.log("Server is upon port 3000");
+});
